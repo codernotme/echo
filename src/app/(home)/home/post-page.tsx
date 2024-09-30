dd45539b97ca7110851f9e563706afb79691d6ac
Aryan <coolaryanbajpai20@gmail.com>
2024-10-01 01:42:57 +0530

    updated

--- src/app/(home)/home/post-page.tsx
+++ src/app/(home)/home/post-page.tsx
@@ -542,18 +542,38 @@ export default function PostPage() {
               size="sm"
               className="text-gray-400 hover:text-red-500"
             >
-              <Heart className="h-5 w-5" />
+              <Heart className="h-8 w-8" strokeWidth={2} />
               <span className="ml-2">{post.post.likesCount || 0}</span>{" "}
               {/* Show likes count */}
             </Button>
-            <Button
-              variant="ghost"
-              size="sm"
-              onClick={() => toggleComments(post.post._id)}
-              className="text-gray-400 hover:text-blue-500"
-            >
-              <MessageCircle className="h-5 w-5 mr-1" />
-            </Button>
+            <div className="group relative">
+              <Button
+                variant="ghost"
+                size="sm"
+                onClick={() => toggleComments(post.post._id)}
+              >
+                <svg
+                  stroke-linejoin="round"
+                  stroke-linecap="round"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  height="44"
+                  width="44"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-8 hover:scale-125 duration-200 hover:stroke-blue-500"
+                  fill="none"
+                >
+                  <path fill="none" d="M0 0h24v24H0z" stroke="none"></path>
+                  <path d="M8 9h8"></path>
+                  <path d="M8 13h6"></path>
+                  <path d="M18 4a3 3 0 0 1 3 3v8a3 3 0 0 1 -3 3h-5l-5 3v-3h-2a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12z"></path>
+                </svg>
+              </Button>
+              <span className="absolute -top-14 left-[50%] -translate-x-[50%] z-20 origin-left scale-0 px-3 rounded-lg border border-gray-300 bg-background py-2 text-sm font-bold shadow-md transition-all duration-300 ease-in-out group-hover:scale-100">
+                Comment <span> </span>
+              </span>
+            </div>
 
             {/* Dialog for sharing post */}
             <Dialog>
